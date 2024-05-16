a6f670e7fb016186a54fc8fca029bd85a9d9d9df
Sean Griffin <sgriffin@janison.com.au>
2024-05-16 23:06:59 +1000

    Add files via upload

---
+++ UI/Fronius/Chart/GetChart.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using Toolkit.Selenium;
+
+namespace UI.Fronius.Chart
+{
+    class GetChart
+    {
+        public static string testMain(
+        )
+        {
+            return WebTestObject.FindUntilDisplayed(
+                null,
+                new By[] {
+                    By.TagName("body"),
+                    By.XPath("./pre")
+                }
+            ).GetText();
+        }
+
+        
+    }
+}
