1c89b4cfbe48b9409d10f44206c7307ed82e4efa
Sean Griffin <sgriffin@janison.com.au>
2024-05-16 23:06:25 +1000

    Add files via upload

---
+++ UI/Fronius/Login/Login.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using Toolkit.Selenium;
+
+namespace UI.Fronius.Login
+{
+    class Login
+    {
+        public static void testMain(
+            string username,
+            string password
+        )
+        {
+
+            WebTestObject.FindUntilDisplayed(
+                null,
+                new By[] {
+                    By.TagName("body"),
+                    By.XPath("//input[@id='usernameUserInput']")
+                }
+            ).SetText(username);
+
+            WebTestObject.FindUntilDisplayed(
+                null,
+                new By[] {
+                    By.TagName("body"),
+                    By.XPath("//input[@id='password']")
+                }
+            ).SetText(password);
+
+            WebTestObject.FindUntilDisplayed(
+                null,
+                new By[] {
+                    By.TagName("body"),
+                    By.XPath("//button[@id='login-button']")
+                }
+            ).click();
+
+            BrowserTestObject.WaitUntilReady();
+        }
+
+        
+    }
+}
